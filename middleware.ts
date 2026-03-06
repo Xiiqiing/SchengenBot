f033adb6ca6ed1967d50669b882e9baf8f111583
XQ <873960774mxq@gmail.com>
2026-03-06 15:35:08 +0100

    fix(i18n): use official next-intl middleware matcher array

--- middleware.ts
+++ middleware.ts
@@ -34,8 +34,13 @@ export default function middleware(req: NextRequest) {
 }
 
 export const config = {
-    // Match all pathnames except for
-    // - … if they start with `/api`, `/_next` or `/_vercel`
-    // - … the ones containing a dot (e.g. `favicon.ico`)
-    matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
+    matcher: [
+        // Enable a redirect to a matching locale at the root
+        '/',
+        // Set a cookie to remember the previous locale for
+        // all requests that have a locale prefix
+        '/(zh|en)/:path*',
+        // Enable redirects that add a locale prefix to requests without one
+        '/((?!api|_next|_vercel|.*\\..*).*)'
+    ]
 };
