f4d350d41ea6da7f42ef82dbff8da9a265e5860e
XQ <873960774mxq@gmail.com>
2026-03-06 15:05:13 +0100

    Fix redirecting

--- next.config.js
+++ next.config.js
@@ -6,6 +6,15 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/zh',
+        permanent: false,
+      },
+    ];
+  },
 }
 
 module.exports = withSentryConfig(
