2f5f8cdcc459fb19fea420af8ea16d9573a3c761
XQ <873960774mxq@gmail.com>
2026-03-14 13:00:37 +0100

    Fix bug

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -8,10 +8,7 @@ import type {
 } from './supabase/types';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseKey =
-  process.env.SUPABASE_SERVICE_ROLE_KEY ||
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
-  '';
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || '';
 
 export const supabase = supabaseUrl && supabaseKey
   ? createClient(supabaseUrl, supabaseKey)
